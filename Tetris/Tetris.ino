642632837c6a1ec922e8ab500321766248ef8ebc
Ladislav Miklík <miklilad@ucebny.adfit.fit.cvut.cz>
2019-04-15 18:40:15 +0200

    Menu, Menu controls, Scoreboard, Credits

--- Tetris/Tetris.ino
+++ Tetris/Tetris.ino
@@ -2,52 +2,160 @@
 #include <SPI.h>
 #include <TFT.h>   
 
-enum States {MENU, GAME, SCOREBOARD};
+const double VERSION = 0.02;
+const int HIGH_SCORES_NUM = 8;
+enum STATES {MENU, GAME, SCOREBOARD, CREDITS};
 int STATE = MENU;
 bool state_changed = true;
+int MenuPos = 50;
+int HighScores[HIGH_SCORES_NUM];
+enum Colors {RED,ORANGE,YELLOW,PURPLE,BLUE,GREEN};
 
 class Block
 {
-  
+  Colors color;
+  bool occupied;
+  public:
+    Block(Colors color = RED,bool occupied = false) : color(color), occupied(false) {}
+
+    Colors Getcolor() const {return color;}
+    bool IsOccupied() const {return occupied;}
+    
 };
 
 class Tetromino
 {
-  
+  protected:
+    byte posX, posY;
+  public:
+    Tetromino(byte x = 5, byte y = 0) : posX(x), posY(y) {}
 };
+
+void play()
+{
+  Block playfield[10][20];
+  byte escape_counter = 0;
+  byte level = 0;
+  byte lines = 0;
+  state_changed=false;
+
+  
+  for(byte x=0; x<10; x++)
+  {
+    for(byte y=0; y<20; y++)
+    {
+      
+    }
+  }
+
+
+  while(true)
+  {
+    int button_pressed = waitForInput();
+    if(button_pressed==SWITCH_RIGHT)
+    {
+      STATE = MENU;
+      state_changed = true;
+    }    
+  }
+
+  STATE = SCOREBOARD;
+}
 
 void display_menu()
 {
   if(state_changed)
   {
+    EsploraTFT.setTextSize(4);
     EsploraTFT.background(0,0,0);
     EsploraTFT.stroke(255,50,50);
-    EsploraTFT.text("Tetris",0,0);
+    EsploraTFT.text("Tetris",8,0);
+    EsploraTFT.stroke(255,255,255);
+    EsploraTFT.setTextSize(1);
+    EsploraTFT.text("Play",50,50);
+    EsploraTFT.text("Scoreboard",50,70);
+    EsploraTFT.text("Credits",50,90);
     state_changed = false;
   }
+  EsploraTFT.text(">",40,MenuPos);
   int button_pressed = waitForInput();
-  if(button_pressed==SWITCH_UP)
-    Esplora.writeRGB(255,255,0);
-  if(button_pressed==SWITCH_DOWN)
-    Esplora.writeRGB(255,0,255);
+  EsploraTFT.stroke(0,0,0);
+  if(button_pressed==SWITCH_RIGHT)
+  {
+    switch(MenuPos)
+    {
+      case 50: STATE = GAME; break;
+      case 70: STATE = SCOREBOARD; break;
+      case 90: STATE = CREDITS; break;
+    }
+    state_changed = true;
+  }
+  if(button_pressed==SWITCH_UP&&MenuPos>50)
+  {
+    EsploraTFT.text(">",40,MenuPos);
+    MenuPos-=20;
+  }
+  if(button_pressed==SWITCH_DOWN&&MenuPos<90)
+  {
+    EsploraTFT.text(">",40,MenuPos);
+    MenuPos+=20;
+  }
+  EsploraTFT.stroke(255,255,255);
   Serial.println(button_pressed);
-}
-
-void play()
-{
-  
 }
 
 void display_scoreboard()
 {
-  
+  if(state_changed)
+  {
+    EsploraTFT.background(0,0,0);
+    EsploraTFT.setTextSize(2);
+    EsploraTFT.text("Scoreboard",0,0);
+    EsploraTFT.setTextSize(1);
+    char text[20];
+    for(byte i=1;i<=HIGH_SCORES_NUM;i++)
+    {
+      String(i).toCharArray(text,20);
+      EsploraTFT.text(text,10,i*10+20); 
+      EsploraTFT.text(":",15,i*10+20);
+      String(HighScores[i-1]).toCharArray(text,20);
+      EsploraTFT.text(text,25,i*10+20);
+    }
+    EsploraTFT.text("> Back",5,EsploraTFT.height()-10);
+    state_changed = false;
+  }
+  int button_pressed = waitForInput();
+  if(button_pressed==SWITCH_RIGHT)
+  {
+    STATE = MENU;
+    state_changed = true;
+  }
 }
 
-void setup()
+void display_credits()
 {
-  EsploraTFT.begin();
-  Serial.begin(9600);
+  if(state_changed)
+  {
+    EsploraTFT.background(0,0,0);
+    EsploraTFT.text("Created by",0,0);
+    EsploraTFT.text("Ladislav Miklik",0,10);
+    EsploraTFT.text("as a semestral work",0,20);
+    EsploraTFT.text("in BI-ARD",0,30);
+    EsploraTFT.text("Current version: ",0,40);
+    char text[5];
+    String(VERSION).toCharArray(text,5);
+    EsploraTFT.text(text,100,40);
+    EsploraTFT.text("> Back",5,EsploraTFT.height()-10);
+    state_changed = false;
+  }
+  int button_pressed = waitForInput();
+  if(button_pressed==SWITCH_RIGHT)
+  {
+    STATE = MENU;
+    state_changed = true;
+  }
 }
+
 
 int waitForInput()
 {
@@ -55,7 +163,7 @@ int waitForInput()
         Esplora.readButton(SWITCH_LEFT)==HIGH&&
         Esplora.readButton(SWITCH_UP)==HIGH&&
         Esplora.readButton(SWITCH_RIGHT)==HIGH){delay(2);}
-  int button = 50;
+  int button = -1;
   if(Esplora.readButton(SWITCH_RIGHT)==LOW)
     button = SWITCH_RIGHT;
   if(Esplora.readButton(SWITCH_LEFT)==LOW)
@@ -72,6 +180,14 @@ int waitForInput()
   return button;
 }
 
+void setup()
+{
+  EsploraTFT.begin();
+  Serial.begin(9600);
+  for(byte i = 0; i < HIGH_SCORES_NUM; i++)
+    HighScores[i] = 0;
+}
+
 void loop()
 {
   if(STATE==MENU)
@@ -80,4 +196,6 @@ void loop()
     play();
   if(STATE==SCOREBOARD)
     display_scoreboard();
+  if(STATE==CREDITS)
+    display_credits();
 }
